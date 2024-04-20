b950503b40f865a125d400b160d2ab3c8c69d8ed
Ashish <ashishmaan22@gmail.com>
2024-04-20 16:20:55 +0530

    ff

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -8,19 +8,20 @@ AItem::AItem() {
 void AItem::BeginPlay() {
 	Super::BeginPlay();
 
-	UWorld* World = GetWorld();
 	
-	
-	SetActorLocation(FVector(0.f, 0.f, 50.f));
-	SetActorRotation(FRotator(0.f, 0.f, 90.f));
-
-	FVector Location = GetActorLocation();
-	FVector Forward = GetActorForwardVector();
-
-	DRAW_SPHERE(Location);
-	DRAW_VECTOR(Location, Location + Forward * 100.f);
 }
 
 void AItem::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
+
+	// Movement rate in units of cm/s
+	float MovementRate = 50.f;
+	float RotationRate = 45.f;
+
+	// Movement rate * deltatime (cm/s) * (s/frame) = (cm/frame)
+	AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
+	AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime,0.f));
+	DRAW_SPHERE_SingleFrame(GetActorLocation());
+	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
+	
 }
