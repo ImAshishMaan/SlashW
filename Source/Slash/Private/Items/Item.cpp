60fd2fc3ee07e953ccdfe14571fa22d524b10b4e
Ashish <ashishmaan22@gmail.com>
2024-04-22 22:39:23 +0530

    collision

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -1,27 +1,54 @@
 #include "Items/Item.h"
-#include "Slash/DebugMacros.h"
+#include "Components/SphereComponent.h"
 
 AItem::AItem() {
 	PrimaryActorTick.bCanEverTick = true;
+
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>("ItemMesh");
+	RootComponent = ItemMesh;
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(GetRootComponent());
+	
 }
 
 void AItem::BeginPlay() {
 	Super::BeginPlay();
 
-	
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnSphereOverlap);
+
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AItem::OnComponentEndOverlap);
 }
 
 void AItem::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 
 	// Movement rate in units of cm/s
-	float MovementRate = 50.f;
+	/*float MovementRate = 50.f;
 	float RotationRate = 45.f;
 
 	// Movement rate * deltatime (cm/s) * (s/frame) = (cm/frame)
 	AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
 	AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime,0.f));
 	DRAW_SPHERE_SingleFrame(GetActorLocation());
-	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
+	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);*/
 	
+}
+
+void AItem::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+
+	const FString OtherActorName = OtherActor->GetName();
+	if(GEngine) {
+		GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Red, OtherActorName);
+	}
+	
+}
+
+void AItem::OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) {
+	const FString OtherActorName = "END OVERLAP";
+	if(GEngine) {
+		GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Red, OtherActorName);
+	}
 }
--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -4,16 +4,32 @@
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
+class USphereComponent;
+class UStaticMeshComponent;
+
 UCLASS()
 class SLASH_API AItem : public AActor {
 	GENERATED_BODY()
 
 public:
 	AItem();
+	virtual void Tick(float DeltaTime) override;
 
 protected:
 	virtual void BeginPlay() override;
 
-public:
-	virtual void Tick(float DeltaTime) override;
+private:
+	
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* ItemMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* Sphere;
+	
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
 };
